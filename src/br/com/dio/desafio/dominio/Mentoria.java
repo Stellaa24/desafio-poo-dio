c7b75caf2bc617b55e8259d222fe7e1955dcc452
Stellaa24 <stellamaris241@gmail.com>
2022-08-08 10:29:04 -0300

    Mentoria e Curso

---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,18 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo;
+    private String descrição;
+    private LocalDate data;
+
+    public Mentoria () {
+    }
+public String getTitulo() {
+        return titulo;
+}
+public void setTitulo(String titulo) {
+
+}
+}
